9e084275fce65ddce5f11007720cc98214bd7df1
LS-plan <2016040219@s.upc.edu.cn>
2023-05-16 14:52:34 +0800

    2023年5月16日上午课程 【任务一】【已完成】ListView的使用，实现列表展示。 [已完成]①创建了list_item_ssy.xml用于描述ListView每个元素的布局 [已完成]②创建了list_view_main_ssy.xml用于描述ListView的整体布局 [已完成]③创建了ListViewSsyActivity.java用于描述ListView的逻辑 【任务二】【进行中】登录页面的编写。 [进行中]①创建了activity_login_ssy.xml用于描述登录页面的布局 [未开始]②创建了LoginSsyActivity.java用于描述登录页面的逻辑 [未开始]②同时上传了石油大学UPC的校徽drawable/upc.jpg 【任务三】【已完成】Intent的使用，实现页面跳转。 [已完成]①在MainActivity的onCreate方法中实现了页面的跳转，逻辑是跳转到List_view_main_ssy.xml页面

---
+++ app/src/main/java/com/example/mobilenettest001/LoginSsyActivity.java
@@ -0,0 +1,17 @@
+package com.example.mobilenettest001;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class LoginSsyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_ssy);
+        Toast.makeText(this,"准备登录...",Toast.LENGTH_SHORT).show();
+    }
+
+}
